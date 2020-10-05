7560a7ce8ccfe04a57625b3039d4d47c6981e9c2
philipplive <info@philipp.live>
2020-10-05 09:44:50 +0200

    Publish

---
+++ cycly-connector.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * CYCLY Connector
+ *
+ * @package           	CYCLY
+ * @author            	CYCLY (Optimanet Schweiz AG)
+ * @copyright         	2020 Optimanet Schweiz AG
+ * @contact        	  	https://cycly.ch/kontakt/
+ *
+ * Plugin Name:       	CYCLY Connector
+ * Plugin URI:        	https://cycly.ch
+ * Description:       	Ermöglicht das Einbinden von Daten aus CYCLY direkt auf Ihre Website.
+ * Version:           	0.9.3
+ * Requires at least: 	7.4
+ * Requires PHP:     	7.4
+ * Author:           	CYCLY (Optimanet Schweiz AG)
+ * Author URI:      	https://optimanet.ch
+ * License:          	Apache License (V2)
+ *
+ * @TODO			 	View für Mitarbeiter
+ */
+
+if (!class_exists('HfCore\System')) {
+	require_once('core/System.php');
+}
+require_once('objects/Item.php');
+require_once('objects/Branch.php');
+require_once('objects/OpeningHours.php');
+require_once('objects/OpeningHoursHour.php');
+require_once('objects/OpeningHoursIrregular.php');
+require_once('CyclyApi.php');
+require_once('CyclySystem.php');
+
+if (is_admin()) {
+	require_once('CyclyBackend.php');
+	new CyclyBackend();
+}
+else {
+	require_once('objects/Image.php');
+	require_once('objects/VehicleCategory.php');
+	require_once('objects/Vehicle.php');
+	require_once('CyclyFrontend.php');
+	new CyclyFrontend();
+}
+
