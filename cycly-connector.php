834e553e3c4991f41f032323860a1e5ce79fb29a
philipplive <philipplive88@gmail.com>
2021-09-24 08:59:10 +0200

    Update

--- cycly-connector.php
+++ cycly-connector.php
@@ -10,7 +10,7 @@
  * Plugin Name:       	CYCLY Connector
  * Plugin URI:        	https://cycly.ch
  * Description:       	Ermöglicht das Einbinden von Daten aus CYCLY direkt auf Ihre Website.
- * Version:           	0.9.9.8
+ * Version:           	0.9.10.0
  * Requires at least: 	5.0
  * Requires PHP:     	7.3
  * Author:           	CYCLY (Optimanet Schweiz AG)
